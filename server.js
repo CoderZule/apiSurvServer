0b50828f2b8e7839763d05381f40605af114f7ea
Mariem Derbali <mariem.derbali20@gmail.com>
2024-10-27 15:40:19 +0100

    .env

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ const io = require('socket.io')(server, {
   cors: {
     origin: 'http://localhost:3001',
     methods: ['GET', 'POST'],
-    credentials: true  // to pass cookies or headers
+    credentials: true  // pass cookies or headers
   }
 });
 
